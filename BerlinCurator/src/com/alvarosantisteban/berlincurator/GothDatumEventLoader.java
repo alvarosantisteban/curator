1c12ca0ecd2326ff641847e354ca8d652676e241
Alvaro <Alvaro@192.168.1.34>
2013-06-07 19:10:26 +0200

    Add a extra link for the clubs in Goth Datum.

--- BerlinCurator/src/com/alvarosantisteban/berlincurator/GothDatumEventLoader.java
+++ BerlinCurator/src/com/alvarosantisteban/berlincurator/GothDatumEventLoader.java
@@ -1,13 +1,37 @@
 package com.alvarosantisteban.berlincurator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.content.Context;
 
 public class GothDatumEventLoader implements EventLoader {
 	
 	private final static String URL = "http://www.goth-city-radio.com/dsb/dates.php";
+	public static final Map<String,String> clubs;
+	static {
+        clubs = new HashMap<String, String>();
+        clubs.put("Berlin - KitKatClub", "http://www.kitkatclub.org/Home/Club/Termine/Index.html");
+        clubs.put("Berlin - K17", "http://www.k17-berlin.de/content.php");
+        clubs.put("Berlin - Red Club","http://www.redclub-berlin.de/RC/");
+        clubs.put("Berlin - Area 61", "https://www.facebook.com/pages/AREA-61/499812456748536");
+        clubs.put("Berlin - Lovelite", "www.lovelite.de");
+        clubs.put("Berlin - Duncker Club", "http://www.dunckerclub.de");
+        clubs.put("Berlin - Sophienclub", "http://www.sophienclub.com");
+        clubs.put("Berlin - SUBVERSIV e.V.", "http://subversiv.squat.net");
+        clubs.put("Berlin - Naherholung Sternchen","http://www.naherholung-sternchen.de/programm");
+        clubs.put("Berlin - What?! Club", "http://www.what-club.de");
+        clubs.put("Berlin - Kulturfabrik (Factory)","http://www.kulturfabrik-moabit.de/kufa");
+        clubs.put("Berlin - White Trash Fast Food", "http://www.whitetrashfastfood.com/events/");
+        clubs.put("Berlin - theARTer", "http://www.thearter.de");
+        clubs.put("Berlin - Periplaneta Kreativzentrum","http://www.periplaneta.com");
+        clubs.put("Berlin - Urban Spree","http://urbanspree.com/blog/events");
+        clubs.put("Berlin - Comet Club","https://www.facebook.com/pages/Comet-Club/257275101014325");
+        clubs.put("Berlin - MS Berlin (Reederrei Bethke)","http://www.reederei-bethke.de/");
+        clubs.put("Berlin - Bi Nuu (Ex-Kato)","https://www.facebook.com/BiNuuBerlin");
+    }
 
 	@Override
 	public List<Event> load(Context context) {
@@ -51,7 +75,7 @@ public class GothDatumEventLoader implements EventLoader {
 				event.setHour(timeAndRest[0].trim());
 				String[] placeAndRest = timeAndRest[1].split("<",2);
 				// Get the place for the description
-				String place = "The event will take place in " +placeAndRest[0] +"<br>"; // In many cases includes the word "Berlin"	
+				String place = "The event will take place in " +placeAndRest[0].trim() +"<br>"; // In many cases includes the word "Berlin"	
 				String[] nothingNameAndRest = placeAndRest[1].split("<i>",2);
 				String[] nameAndRest = nothingNameAndRest[1].split("<",2);
 				// Set the name
@@ -60,9 +84,28 @@ public class GothDatumEventLoader implements EventLoader {
 				String[] nothingAndDescription = nameAndRest[1].split("<BR>",2); // Si quito el 2, lo puedo tener dividido en parrafos e iterar con for
 				String[] description = nothingAndDescription[1].split("</TD>");
 				event.setDescription(place +description[0]);
+				// Add a extra link if possible
+				String link = addExtraLink(placeAndRest[0].trim());
+				if (!link.equals("no link")){
+					event.setLink(link);
+				}
 				events.add(event);
 			}
 		}
 		return events;		
+	}
+
+	/**
+	 * Checks if there is a corresponding link for the clubName parameter
+	 * 
+	 * @param clubName the name of the club. It can include other words
+	 * @return the corresponding link, if applies. If not, returns "no link"
+	 */
+	private String addExtraLink(String clubName) {
+		//System.out.println("clubName="+clubName +".");
+		if (clubs.containsKey(clubName)){
+			return clubs.get(clubName);
+		}
+		return "no link";
 	}
 }
